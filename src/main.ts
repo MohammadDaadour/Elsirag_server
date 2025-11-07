bdee6c4fa22cc4c8f694745b39846a7ccb48840e
MohammadDaadour <daadourmohammad@gmail.com>
2025-11-07 02:11:01 +0200

    m

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,11 @@ async function bootstrap() {
   app.use(cookieParser());
 
   app.enableCors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    origin: [
+      process.env.FRONTEND_URL || 'http://localhost:3000',
+      'https://www.elsirag.com',
+      'https://elsirag.com'
+    ],
     credentials: true,
   });
 
