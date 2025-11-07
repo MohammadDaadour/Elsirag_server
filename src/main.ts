c57911eb9c309b64f8328cb3c905c37df05e57d6
MohammadDaadour <daadourmohammad@gmail.com>
2025-11-07 07:04:51 +0200

    m

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from '../src/app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
-import { Logger } from '@nestjs/common';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
 import * as bodyParser from 'body-parser';
 
-import { IncomingMessage } from 'http';
+const server = express();
+
+let app;
 
 declare module 'http' {
   interface IncomingMessage {
@@ -13,39 +16,99 @@ declare module 'http' {
   }
 }
 
-async function bootstrap() {
-  Logger.overrideLogger(['log', 'error', 'warn', 'debug', 'verbose']);
-  const app = await NestFactory.create(AppModule);
+async function createApp() {
+  if (!app) {
+    app = await NestFactory.create(
+      AppModule,
+      new ExpressAdapter(server),
+      { logger: ['log', 'error', 'warn', 'debug', 'verbose'] }
+    );
 
-  app.use(cookieParser());
+    app.use(cookieParser());
 
-  app.enableCors({
-    origin: [
-      process.env.FRONTEND_URL || 'http://localhost:3000',
-      'https://www.elsirag.com',
-      'https://elsirag.com'
-    ],
-    credentials: true,
-  });
+    app.enableCors({
+      origin: [
+        process.env.FRONTEND_URL || 'http://localhost:3000',
+        'https://www.elsirag.com',
+        'https://elsirag.com'
+      ],
+      credentials: true,
+    });
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true,
-    }),
-  );
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    );
 
-  app.use(
-    bodyParser.json({
-      verify: (req, res, buf) => {
-        req.rawBody = buf;
-      },
-    }),
-  );
+    app.use(
+      bodyParser.json({
+        verify: (req, res, buf) => {
+          req.rawBody = buf;
+        },
+      }),
+    );
 
-  await app.listen(process.env.PORT ?? 3200);
-
+    await app.init();
+  }
+  return app;
 }
-bootstrap();
+
+export default async (req, res) => {
+  await createApp();
+  server(req, res);
+};
+
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import { ValidationPipe } from '@nestjs/common';
+// import * as cookieParser from 'cookie-parser';
+// import { Logger } from '@nestjs/common';
+// import * as bodyParser from 'body-parser';
+
+// import { IncomingMessage } from 'http';
+
+// declare module 'http' {
+//   interface IncomingMessage {
+//     rawBody?: Buffer;
+//   }
+// }
+
+// async function bootstrap() {
+//   Logger.overrideLogger(['log', 'error', 'warn', 'debug', 'verbose']);
+//   const app = await NestFactory.create(AppModule);
+
+//   app.use(cookieParser());
+
+//   app.enableCors({
+//     origin: [
+//       process.env.FRONTEND_URL || 'http://localhost:3000',
+//       'https://www.elsirag.com',
+//       'https://elsirag.com'
+//     ],
+//     credentials: true,
+//   });
+
+//   app.useGlobalPipes(
+//     new ValidationPipe({
+//       whitelist: true,
+//       forbidNonWhitelisted: true,
+//       transform: true,
+//     }),
+//   );
+
+//   app.use(
+//     bodyParser.json({
+//       verify: (req, res, buf) => {
+//         req.rawBody = buf;
+//       },
+//     }),
+//   );
+
+//   await app.listen(process.env.PORT ?? 3200);
+
+// }
+// bootstrap();
 
